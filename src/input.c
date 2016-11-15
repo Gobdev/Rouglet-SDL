ae5b17ea50e071fdfa0f01a05db9562c131a2367
kawaji <kawaji@kth.se>
2016-11-15 11:32:04 +0100

    fixed input.c to have the correct checks for the buttons and switches

--- src/input.c
+++ src/input.c
@@ -1,27 +1,30 @@
 #include <stdint.h>
 #include <pic32mx.h>
 
-int getButtons(void){ //bit 4-7 in port D
+/*
+int getButtons(void){ //bit 1 in port F and bit 5-7 in port D
 	return (PORTD & 0xE0) >> 5;
 }
+*/
 
+/*
 int getSwitches(void){ //bit 8-11 in port D
 	return (PORTD & 0xF00) >> 8;
 }
-
+*/
 char checkButton(char buttonNumber){
 	switch(buttonNumber){
 		case 1:
-			return (getBtns() & 0x10);
+			return (PORTF & 0x2); //bit index 1 in PORTF
 			break;
 		case 2:
-			return (getBtns() & 0x20);
+			return (PORTD & 0x20); //bit index 5 in PORTD
 			break;
 		case 3:
-			return (getBtns() & 0x40);
+			return (PORTD & 0x40); //bit index 6 in PORTD
 			break;
 		case 4:
-			return (getBtns() & 0x80);
+			return (PORTD & 0x80); //bit index 7 in PORTD
 			break;
 	}
 }
@@ -29,16 +32,16 @@ char checkButton(char buttonNumber){
 char checkSwitches(char switchNumber){
 	switch(switchNumber){
 		case 1:
-			return (getSwitches & 0x80);
+			return (PORTD & 0x80); //bit index 8 in PORTD
 			break;
 		case 2:
-			return (getSwitches & 0x100);
+			return (PORTD & 0x100);//bit index 9 in PORTD
 			break;
 		case 3:
-			return (getSwitches & 0x200);
+			return (PORTD & 0x200);//bit index 10 in PORTD
 			break;
 		case 4:
-			return (getSwitches & 0x400);
+			return (PORTD & 0x400);//bit index 11 in PORTD
 			break;	
 	}
 }
