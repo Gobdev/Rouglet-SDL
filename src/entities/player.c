5b2d9560b63e39d127ca423e0bbbd635b601e05a
kawaji <kawaji@kth.se>
2016-12-08 14:35:33 +0100

    Added inventory features and weapons

--- src/entities/player.c
+++ src/entities/player.c
@@ -4,21 +4,55 @@
 #include "../images/UI.h"
 #include "../images/character_sprites.h"
 
-enum items {POTION,BOMB,SWORD1,SWORD2,SWORD3,SPEAR1,SPEAR2,HAMMER,GREATSWORD,PICKAXE,AXE,HALBERD,SMALL_AXE,TRIDENT,STAFF,BATTLE_AXE};
+enum items {SWORD1,SWORD2,SWORD3,SPEAR1,SPEAR2,HAMMER,GREATSWORD,PICKAXE,AXE,HALBERD,SMALL_AXE,TRIDENT,STAFF,BATTLE_AXE,POTION,BOMB};
 
-int player_hp = 160;
+int weapon_low_list[] = {10,20,30,40,50,60,70,80,90,100,110,120,130,140};
+int weapon_high_list[] = {10,20,30,40,50,60,70,80,90,100,110,120,130,140};
+
 int player_max_hp = 160;
+int player_hp = player_max_hp;
 int player_atk_low = 135;
 int player_atk_high = 165;
+int weapon_atk_low = 0;
+int weapon_atk_high = 0;
 int player_gold = 0;
 int player_level = 1;
 int player_x = 64;
 int player_y = 12;
 int inventory_size = 15;
-char inventory[inventory_size] = {0};
+int potion_heal_amount = 50;
+int inventory[inventory_size] = {0};
+int active_weapon_index = 0;
 
+void set_current_weapon(int low,int high,int item_index){
+    weapon_atk_low = attack_low;
+    weapon_atk_high = attack_high;
+    active_weapon_index = item_index;
+}
 
-void add_to_inventory(char item_id){
+void draw_active_weapon(){
+    //TODO
+}
+
+void use_item(int item_index){
+    int item_id = inventory[index];
+    switch(){
+        case POTION:
+            if(player_hp + potion_heal_amount >= player_max_hp){
+                player_hp = player_max_hp;
+            }else{
+                player_hp += potion_heal_amount;
+            }
+            remove_item(item_index);
+            break;
+        case BOMB:
+            break; //TODO
+        case default:
+            set_current_weapon(weapon_low_list[item_id], weapon_high_list[item_id], item_index);
+    }
+}
+
+void add_to_inventory(int item_id){
     for(int i = 0 ; i < inventory_size ; i++){
         if(inventory[i] == 0){
             inventory[i] = item_id;
@@ -56,9 +90,9 @@ void player_level_up(){
 
 void player_draw_ui(){
     paint_pic(0, 0, ui1);
-    print_int(10, 2, player_atk_low);
+    print_int(10, 2, (player_atk_low + weapon_atk_low));
     print_text(22, 2, "-");
-    print_int(26, 2, player_atk_high);
+    print_int(26, 2, (player_atk_high + weapon_atk_high));
     print_int(10, 10, player_hp);
     print_text(22, 10, "/");
     print_int(26, 10, player_max_hp);
