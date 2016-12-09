32d4791d73741ed61d70aa13990961fcddc754ab
kawaji <kawaji@kth.se>
2016-12-09 18:22:44 +0100

    Slight changes to enemy stats

--- src/entities/enemy_arrays.h
+++ src/entities/enemy_arrays.h
@@ -6,17 +6,16 @@ const int enemy_array[] = {
 	30,        	30,         20,        25,          7,          (int) fireKid,
 	60,         60,       	6,         17,          9,          (int) frog,
 	50,        	50,         15,        40,          15,         (int) fetus,
-
+	55,        	55,        	5,         9,           6,          (int) mrEye,
     170,        170,        10,        45,          24,         (int) alien,
     300,        300,        25,        60,          43,         (int) alienKing,
     80,        	80,         4,         8,           6,          (int) doomSlime,
     150,        150,     	30,        40,          12,         (int) floatingEye,
-    55,        	55,        	5,         9,           4,          (int) mrEye,
-    200,        200,        30,        40,          33,         (int) spooks,
     90,         90,         24,        40,          15,         (int) skeleton,
     110,        110,        26,        36,          21,         (int) flower_man,
-    240,        240,        20,         34,         20,         (int) evil_thing,
-    300,        300,        25,        30,          32,         (int) oni,
-    400,        400,       	40,        70,          61,         (int) knight,
+    200,        200,        30,        40,          33,         (int) spooks,
+    240,        240,        20,         34,         32,         (int) evil_thing,
+    300,        300,        25,        30,          45,         (int) oni,
+    600,        600,       	40,        70,          61,         (int) knight,
     1000,       1000,       80,        120,         220,        (int) spider_face
 };
