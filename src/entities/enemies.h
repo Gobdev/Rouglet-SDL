acb0a5dd814bab9b41be4853e836edaf83744072
Johan Ribom <jribom@kth.se>
2016-12-09 14:30:10 +0100

    Added support for providing enemy with x- and y-coordinate.

--- src/entities/enemies.h
+++ src/entities/enemies.h
@@ -1,9 +1,10 @@
 #pragma once
 
+char enemy_is_enabled(int enemy);
 int enemy_get_x(int enemy);
 int enemy_get_y(int enemy);
 void delete_enemy(int enemy);
-void init_enemy(int enemy, int type);
+void init_enemy(int enemy, int type, int x, int y);
 void enemy_draw(int enemy, int x, int y);
 void enemy_damage_player(int enemy);
 void enemy_take_damage(int enemy, int damage);
